9b3dfe3b9dde941e574826b930aa2ac85027c683
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-07-22 09:18:28 +1000

    Updated from private repository

---
+++ Selkie.Services.Common/Installer.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Selkie.Windsor;
+
+namespace Selkie.Services.Common
+{
+    [ExcludeFromCodeCoverage]
+    //ncrunch: no coverage start
+    public class Installer : BaseInstaller <Installer>
+    {
+    }
+}
